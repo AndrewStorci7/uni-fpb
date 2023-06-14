31f53ad8aff5d9aadbdc11051c5a4cf0e17d3b40
Andrea Storci <s.andrea.storci@gmail.com>
2023-06-14 11:38:30 +0200

    Add text exercise

--- ProvaEsame250522/main.cpp
+++ ProvaEsame250522/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * SIMLUAZIONE ESAME 25/05/2022 ___ ESRCIZIO 1[C++]: (15 pt.):
+ *
+ * Definire una classe templatica Inventory che rappresenta un inventario di elementi di tipo T.
+ * La classe deve definire un costruttore senza argomenti che crea un inventario vuoto. Il metodo add aggiunge un
+ * elemento di tipo T all’inventario. Il metodo count prende come argomento un elemento di tipo T e restituisce il numero
+ * di occorrenza dell’elemento passato come argomento. Il metodo getMostCommon restituisce l’elemento che occorre pi`u
+ * spesso nell’inventario. Un inventario ha dimensione indefinita. Non `e consentito usare la STL. Se necessario, ridefinire gli
+ * opportuni metodi, costruttori e/o operatori.
+ *
+ * Inventory < int > inv0 ;
+ * Inventory < string > inv1 ;
+ * inv0 . add (1); inv0 . add (2); inv0 . add (1); inv0 . add (3);
+ * inv1 . add ( " hello " ); inv1 . add ( " hello " ); inv1 . add ( " ciao " ); inv1 . add ( " hello " );
+ * cout << inv0 . count (7) << endl ; // output : 0
+ * cout << inv0 . count (1) << endl ; // output : 2
+ * cout << inv1 . count ( " ciao " ) << endl ; // output : 1
+ * cout << inv0 . getMostCommon () << endl ; // output : 1
+ *
+ */
+
+
+
 template <typename T>
 class Inventory {
 private:
